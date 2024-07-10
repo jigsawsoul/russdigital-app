e2eef47fd398a67e384bf9ced2203575322ee2f3
r-howse <richard.howse@mvfglobal.com>
2024-07-10 01:10:13 +0100

    Add page change progress bar

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,5 @@
 import "../faust.config";
-import React from "react";
-import { useEffect } from "react";
+import React, { useEffect } from "react";
 import { useRouter } from "next/router";
 import { FaustProvider } from "@faustwp/core";
 import NProgress from "nprogress";
@@ -8,30 +7,26 @@ import "nprogress/nprogress.css";
 import "@faustwp/core/dist/css/toolbar.css";
 import "../styles/global.scss";
 
-export default function MyApp({ Component, pageProps }) {
-  const router = useRouter();
-
+const useRouteProgress = (router) => {
   useEffect(() => {
-    const handleRouteChangeStart = () => {
-      NProgress.start();
-    };
-    const handleRouteChangeComplete = () => {
-      NProgress.done();
-    };
-    const handleRouteChangeError = () => {
-      NProgress.done();
-    };
+    const handleRouteChangeStart = () => NProgress.start();
+    const handleRouteChangeEnd = () => NProgress.done();
 
     router.events.on("routeChangeStart", handleRouteChangeStart);
-    router.events.on("routeChangeComplete", handleRouteChangeComplete);
-    router.events.on("routeChangeError", handleRouteChangeError);
+    router.events.on("routeChangeComplete", handleRouteChangeEnd);
+    router.events.on("routeChangeError", handleRouteChangeEnd);
 
     return () => {
       router.events.off("routeChangeStart", handleRouteChangeStart);
-      router.events.off("routeChangeComplete", handleRouteChangeComplete);
-      router.events.off("routeChangeError", handleRouteChangeError);
+      router.events.off("routeChangeComplete", handleRouteChangeEnd);
+      router.events.off("routeChangeError", handleRouteChangeEnd);
     };
   }, [router]);
+};
+
+export default function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+  useRouteProgress(router);
 
   return (
     <FaustProvider pageProps={pageProps}>
