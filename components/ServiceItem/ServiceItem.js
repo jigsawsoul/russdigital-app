5af1316019a5d24cfe51d6073be3a277c5be860e
r-howse <richard.howse@mvfglobal.com>
2024-11-04 10:08:32 +0000

    Update icon to use image component

--- components/ServiceItem/ServiceItem.js
+++ components/ServiceItem/ServiceItem.js
@@ -1,10 +1,17 @@
 import Link from "next/link";
+import Image from "next/image";
 
 export default function ServiceItem({ item, icon }) {
   return (
     <div className="bg-[#F6F5F3] py-10 px-9 font-body flex flex-col lg:flex-row gap-8 lg:gap-24 h-full">
       <div className="lg:order-2">
-        <img src={icon} className="min-w-10 min-h-10" width="40" height="40" />
+        <Image
+          src={icon}
+          className="min-w-10 min-h-10"
+          width="40"
+          height="40"
+          alt="Service Icon"
+        />
       </div>
       <div className="">
         <h2 className="mb-2 lg:mb-4 font-title font-semibold text-xl lg:text-2xl">
